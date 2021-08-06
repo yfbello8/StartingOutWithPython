af085177ee448eac8c5452e55b4419e4d6145fa2
Yusuf Bello <yfbello8@gmail.com>
2021-08-05 20:49:24 -0700

    Started setting up chapter 4 and fixed the template

---
+++ Chapter 4/Chapter_4.py
@@ -0,0 +1,202 @@
+print("Chapter 4 - Repetition Structures")
+print("This program displays the Programming Challenges in Chapter 4")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 1 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 2 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 3 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 4 - ")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 5 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 6 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 7 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 8 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 9 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 10 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 11 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 12 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 13 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 14 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 15 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 16 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 17 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 18 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+print("Question 19 - ")
+print("")
+print("")
+print("")
+print("")
+print("")
+
+enter = input ("Press enter to continue")
+print("")
+
+
+print("Thanks for checking out Programming Assignments for Chapter 4! ")
+print("Check out some of the other Programming Assignment I have up.")
+print("Goodbye!")
+
